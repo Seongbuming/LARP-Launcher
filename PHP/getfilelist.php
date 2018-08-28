2fa11673a7f4e7c98600675e24604bfc3c4a39dd
SeongbumSeo <sBum.Seo@gmail.com>
2018-08-28 12:08:10 +0900

    구조 변경

--- PHP/getfilelist.php
+++ PHP/getfilelist.php
@@ -1,16 +1,16 @@
 <?php
-$rpath = $_SERVER['DOCUMENT_ROOT'].'/Launcher/'.$_GET['type'].'/Files';
+$rpath = $_GET['type'];
 $rarr = get_file_list($rpath);
 for($i = 0; $i < sizeof($rarr); $i++)
 {
-	$filename = substr($rarr[$i],strlen($rpath)+1);
-	$filehash = hash_file('md5','./'.$_GET['type'].'/Files/'.$filename);
-	echo str_replace('/','\\',$filename).','.$filehash;
+	$filename = substr($rarr[$i], strlen($rpath)+1);
+	$filehash = hash_file('md5', './'.$_GET['type'].'/'.$filename);
+	echo str_replace('/','\\', $filename).','.$filehash;
 	if($i != sizeof($rarr)-1)
 		echo '|';
 }
 
-function get_file_list($path,$arr=array())
+function get_file_list($path, $arr=array())
 {
 	$dir = opendir($path);
 	while($file = readdir($dir))
@@ -18,7 +18,7 @@ function get_file_list($path,$arr=array())
 		if($file == '.' || $file == '..')
 			continue;
 		else if(is_dir($path.'/'.$file))
-			$arr = get_file_list($path.'/'.$file,$arr);
+			$arr = get_file_list($path.'/'.$file, $arr);
 		else
 			$arr[] = $path.'/'.$file;
 	}
